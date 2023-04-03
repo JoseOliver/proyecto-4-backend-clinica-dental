d347d8e3f2c200987d3d0efcd0351896908bfaa2
Jose Andrés <oliver_ja@outlook.com>
2023-04-03 13:56:25 +0200

    fix: last commit error on connecting resolved

--- db/db.js
+++ db/db.js
@@ -1,6 +1,7 @@
 const dotenv = require('dotenv');
 dotenv.config();
 const {Sequelize, DataTypes} = require('sequelize');
+console.log(process.env.MYSQL_HOST)
 const sequelize = new Sequelize(
     process.env.MYSQL_DATABASE, 
     process.env.MYSQL_USERNAME, 
